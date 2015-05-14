0a5b40049ae4bc3db4a57c031a7676408dc10ccd
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-14 15:07:10 +0600

    version 02

--- app/src/main/java/kz/abcsoft/listfragmentdemo1/MainList.java
+++ app/src/main/java/kz/abcsoft/listfragmentdemo1/MainList.java
@@ -1,7 +1,18 @@
 package kz.abcsoft.listfragmentdemo1;
 
 import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainList extends ListFragment {
+    String [] data = new String[]{"one", "two", "three", "four"} ;
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, data) ;
+        setListAdapter(adapter);
+    }
 }
