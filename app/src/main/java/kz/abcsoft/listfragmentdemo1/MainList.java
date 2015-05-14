362585a1f31fb1ccaff969bd351641242e9c821b
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-14 15:14:42 +0600

    version 03

--- app/src/main/java/kz/abcsoft/listfragmentdemo1/MainList.java
+++ app/src/main/java/kz/abcsoft/listfragmentdemo1/MainList.java
@@ -2,6 +2,9 @@ package kz.abcsoft.listfragmentdemo1;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 public class MainList extends ListFragment {
@@ -14,5 +17,11 @@ public class MainList extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, data) ;
         setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, container, false);
     }
 }
